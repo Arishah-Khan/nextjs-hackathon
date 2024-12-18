308d9bd4a60c2142b86fd3d3871017ebf71780e3
Arishah Khan <arishahsanaullah78@gmail.com>
2024-12-18 19:39:13 +0500

    Changes

--- src/app/(group)/shop/page.tsx
+++ src/app/(group)/shop/page.tsx
@@ -1,12 +1,12 @@
-import BlogPage from "@/components/blog/card";
 import HeroSection from "@/components/menu/heroSec";
+import CardsPage from "@/components/shop/card-page";
 
 export default function Shop(){
     return(
         <main className="max-w-[1340px]  mx-auto">
 <div className="bg-[#0d0d0d]">
              <HeroSection pageTitle="Shop" page="Shop"/></div>
-             <BlogPage />
+             <CardsPage />
         </main>
     )
 }
