4d6d9aef5bb170f7ffafe0c16e24af2e100f4d2f
Arishah Khan <arishahsanaullah78@gmail.com>
2025-01-31 20:04:23 +0500

    Changes

--- src/app/(group)/payment-success/page.tsx
+++ src/app/(group)/payment-success/page.tsx
@@ -1,10 +1,12 @@
-interface IParams {
-  searchParams: {
-    amount: number;
-  };
-}
 
-const PaymentSuccess = ({ searchParams }: IParams) => {
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+
+const PaymentSuccess = () => {
+  const searchParams = useSearchParams();
+  const amount = searchParams.get('amount');  
+
   return (
     <div className="flex flex-col justify-center items-center w-full py-16 px-4 bg-green-100">
       <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold text-green-700 mb-4">
@@ -13,7 +15,7 @@ const PaymentSuccess = ({ searchParams }: IParams) => {
       <p className="text-xl sm:text-2xl text-gray-700 mb-6">
 {`        We're excited to serve you! Your total order amount is
 `}        <span className="text-xl sm:text-2xl font-semibold text-orange-500">
-          $ {searchParams.amount}
+          ${amount}
         </span>
       </p>
       <div className="bg-white p-6 rounded-lg shadow-md w-full sm:w-3/4 md:w-1/2 lg:w-1/3">
